d502665644e1a74bbfcef4bc3e145015a2e1c19f
Sonali <Sonali@DESKTOP-06CFBTH>
2022-05-30 14:56:54 +0530

    updated ecelfiledata.java

--- src/main/java/com/sdet34l1/genericLibrary/ExcelFileData.java
+++ src/main/java/com/sdet34l1/genericLibrary/ExcelFileData.java
@@ -13,7 +13,7 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 /**
  * This class contains excel related common data
  * @author Sonali
- *This excel class is updated.....
+ *This excel class is updated..... ..30 may
  */
 
 public class ExcelFileData {
